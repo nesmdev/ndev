e43a5bdaa56e1691fd88864a14e440e10632cc75
ndev <ndev>
2021-02-02 17:57:04 -0500

    nurl

---
+++ ndev.js
@@ -0,0 +1,33 @@
+class nurl {
+	constructor(url) {
+		if (typeof url == "string") url = new URL(url);
+		this.url = url;
+		this.params = new URLSearchParams(url.search);
+	}
+
+	set(key, param) {
+		this.params.set(key, param);
+		this.url.search = this.params.toString();
+		return this;
+	}
+
+	get(key) {
+		return this.params.get(key);
+	}
+
+	getHash() {
+		return this.url.hash;
+	}
+
+	toggleHash(hash, toggle) {
+		if (toggle) {
+			this.url.hash = hash;
+		} else {
+			this.url.hash = null;
+		}
+		return this;
+	}
+	toString() {
+		return this.url.toString();
+	}
+}
